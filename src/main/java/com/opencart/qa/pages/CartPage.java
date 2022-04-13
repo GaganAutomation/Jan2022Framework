8d680aac52ae152910969d0af8b64b4e7e9a8270
Gagan Deep Tyagi <gagan.tyagi@QUALITEST.LOCAL>
2022-04-13 17:19:44 +0530

    Added cart Page

---
+++ src/main/java/com/opencart/qa/pages/CartPage.java
@@ -0,0 +1,15 @@
+package com.opencart.qa.pages;
+
+public class CartPage {
+	
+	public void cartPage() {
+		System.out.println("Add to Cart");
+	}
+	
+	public static void main(String[] args) {
+		
+		CartPage c = new CartPage();
+		c.cartPage();
+	}
+
+}
