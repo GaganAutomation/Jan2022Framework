064be652d4b49e19ee37a67b5ce637cdd28f4f35
GaganAutomation <103435953+GaganAutomation@users.noreply.github.com>
2022-04-16 20:32:21 +0530

    Create Sample.java

---
+++ src/main/java/com/opencart/qa/pages/Sample.java
@@ -0,0 +1,16 @@
+package com.opencart.qa.pages;
+
+public class Sample {
+	
+	public void login() {
+		String name = "Naveen";
+    String pwd = "naveen123";
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
