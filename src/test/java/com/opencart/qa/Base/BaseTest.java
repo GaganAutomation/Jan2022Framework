95171a2b3d519b811964115b6959d959739a4f5e
Gagan Deep Tyagi <gagan.tyagi@QUALITEST.LOCAL>
2022-05-05 14:57:46 +0530

    added @optional in BaseTest

--- src/test/java/com/opencart/qa/Base/BaseTest.java
+++ src/test/java/com/opencart/qa/Base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
@@ -29,9 +30,10 @@ public class BaseTest {
 	public SoftAssert sofAssert;
 	
 
+	
 	@Parameters({"browser"})
 	@BeforeTest
-	public void setup(String browser) {
+	public void setup(@Optional String browser) {
 		df = new DriverFactory();
 		prop = df.init_properties();
 		
